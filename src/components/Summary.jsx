78f62e68535485497a7ce848851c93ce39c1e1fe
hakan.basturknewinn.com <forxynet@gmail.com>
2024-09-13 12:59:09 +0300

    updated summary 2

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -2,21 +2,29 @@ import quizCompleteImg from '../assets/quiz-complete.png';
 import QUESTIONS from '../questions.js';
 
 export default function Summary({ userAnswers }) {
+    const skippedAnswers = userAnswers.filter(answer => answer === null);
+    const corretAnswers = userAnswers.filter((answer, index) => answer === QUESTIONS[index].answers[0]);
+    const incorretAnswers = userAnswers.filter((answer, index) => answer !== QUESTIONS[index].answers[0]);
+    
+    const skippedAnswersShare = Math.round((skippedAnswers.length / userAnswers.length) * 100);
+    const currectdAnswersShare = Math.round((corretAnswers.length / userAnswers.length) * 100);
+    const wrongAnswersShare = 100 - skippedAnswers - currectdAnswersShare;
+
     return (
         <div id="summary">
             <img src={quizCompleteImg} alt="Trophy icon" />
             <h2>Quiz Completed!</h2>
-            <div id="summary-starts">
+            <div id="summary-stats">
                 <p>
-                    <span className='number'>10%</span>
+                    <span className='number'>{skippedAnswersShare}%</span>
                     <span className='text'>skipped</span>
                 </p>
                 <p>
-                    <span className='number'>10%</span>
-                    <span className='text'>answere correctly</span>
+                    <span className='number'>{currectdAnswersShare}%</span>
+                    <span className='text'>answered correctly</span>
                 </p>
                 <p>
-                    <span className='number'>10%</span>
+                    <span className='number'>{wrongAnswersShare}%</span>
                     <span className='text'>answered incorrectly</span>
                 </p>
             </div>
